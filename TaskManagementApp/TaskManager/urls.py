28435157d68fd8e436236b4a40456e69393fe7c7
issac-kondreddy <ixk93660@ucmo.edu>
2024-09-19 15:36:06 -0500

    Editing and deleting views are added and styling of tasks is done

--- TaskManagementApp/TaskManager/urls.py
+++ TaskManagementApp/TaskManager/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('tasks/',views.views_tasks,name= 'task_list'),
     path('tasks/add', views.add_task, name='add_task'),
+    path('tasks/edit/<int:task_id>/', views.edit_task, name='edit_task'),
+    path('tasks/delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
--- TaskManagementApp/TaskManager/views.py
+++ TaskManagementApp/TaskManager/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Tasks
-
-# Create your views here.
-from .models import Tasks
-
+from django.utils import timezone
 def views_tasks(request):
-    tasks = Tasks.objects.all().values()
+    tasks = Tasks.objects.filter(deleted_at__isnull=True)
     template = loader.get_template('TaskManager/task_list.html')
     context = {
         'tasks': tasks,
@@ -24,3 +21,22 @@ def add_task(request):
         return redirect('task_list')
     else:
         return render(request, 'TaskManager/add_task.html')
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Tasks, pk=task_id)
+
+    if request.method == 'POST':
+        task.task_name = request.POST['task_name']
+        task.task_description = request.POST['task_description']
+        task.task_status = request.POST['task_status']
+        task.save()  # Save the updated task object to the database
+        return redirect('task_list')
+
+    return render(request, 'TaskManager/edit_task.html', {'task': task})
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Tasks, pk=task_id)
+    task.deleted_at = timezone.now()
+    task.save()
+    return redirect('task_list')
+
